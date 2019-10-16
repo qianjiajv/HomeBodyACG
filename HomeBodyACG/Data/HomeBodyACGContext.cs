0711bd6f7a59d47b210188543ca629181bbcee72
qian <qianjiajuyouxiang@163.com>
2019-10-16 18:29:13 +0800

    qian-10/16

---
+++ HomeBodyACG/Data/HomeBodyACGContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HomeBodyACG.Models;
+
+namespace HomeBodyACG.Data
+{
+    public class HomeBodyACGContext : DbContext
+    {
+        public HomeBodyACGContext (DbContextOptions<HomeBodyACGContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Animations> Animations { get; set; }
+    }
+}
